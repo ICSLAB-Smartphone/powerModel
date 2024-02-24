7ce0a4f814c2c45191d70f902ea053f0742187f1
hacksang <985438046@qq.com>
2024-02-24 17:26:19 +0800

    add support for usb connect and su without adb_root

--- src/device.py
+++ src/device.py
@@ -1,6 +1,10 @@
+import os
 from print_color import print
 from adb_shell import adb_device
 from adb_shell.adb_device import AdbDeviceUsb,AdbDeviceTcp
+from adb_shell.auth.keygen import keygen
+from adb_shell.auth.sign_pythonrsa import PythonRSASigner
+from src.log import logger
 from configs import config
 import subprocess
 
@@ -10,11 +14,35 @@ class Device:
 		When we measure the power of the device, we must disconnect the usb with the device
 		So we use tcp connect for convenience
 	'''
-	def __init__(self, ip, port, timeout=10):
+	def __init__(self, ip, port, timeout=10, usb_flag=False):
+		# Load the public and private keys
+		adbkey = './adbkey'
+		if not os.path.exists(adbkey):
+			keygen(adbkey)
+		with open(adbkey) as f:
+			priv = f.read()
+		with open(adbkey + '.pub') as f:
+			 pub = f.read()
+		signer = PythonRSASigner(pub, priv)
 		adb_device._maxdata = 2048
-		self._device = AdbDeviceTcp(ip, port, default_transport_timeout_s=timeout) 
-		self._device.connect()
-		self._device.root()
+		
+		self._usb_flag = usb_flag
+		if usb_flag:
+			#self._device = AdbDeviceUsb() 
+			### adb_shell lib cannot run parallel with "adb shell command"
+			pass
+		else:
+			self._device = AdbDeviceTcp(ip, port, default_transport_timeout_s=timeout) 
+			self._device.connect(rsa_keys=[signer])
+			self._device.root()
+
+		output = self.run_shell_cmd('root')
+		if 'cannot' in output:
+			logger.warning("Adb root failed")
+			self._root = False
+		else:
+			logger.debug("Adb root success")
+			self._root = True
 
 		''' Get basic information using adb shell '''
 		self._system_version = self.run_shell_cmd("getprop ro.build.version.release")
@@ -56,7 +84,22 @@ class Device:
 		print(self._mem_total + " GB", tag="Mem total", tag_color='green', color='white')
 
 	def run_shell_cmd(self, cmd):
-		return self._device.shell(cmd).strip()
+		if 'root' in cmd:
+			info = subprocess.check_output(['adb', 'root'])
+			return info.decode('utf-8').strip()
+		elif self._usb_flag:	#non root command we all use su to execute
+			if not self._root:
+				cmds = ['su', cmd, 'exit']
+			else:
+				cmds = [cmd]
+			obj = subprocess.Popen("adb shell", shell= True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+			info = obj.communicate(("\n".join(cmds) + "\n").encode('utf-8'));
+			return info[0].decode('utf-8').strip()
+		else:
+			if not self._root:
+				return self._device.shell("su -c \'" + cmd + " \'").strip()
+			else:
+				return self._device.shell(cmd).strip()
 
 	def push_file(self, src, dest):
 		subprocess.run(["adb", "push", src ,dest])
